9ce33358653d3f5ffdec2ef682ecc9ce5f0bb424
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-16 16:29:14 -0300

    cria dropdown de trocar de idioma

--- ts/src/app/pagina-principal/header/header.component.ts
+++ ts/src/app/pagina-principal/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  opcoesDeIdioma = ['PT, BR', 'EN-US', 'ES-MX', 'FR-FR', 'DE-DE']
 
   constructor() { }
 
