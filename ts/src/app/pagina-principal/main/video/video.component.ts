b3bef9723bde2b4644e81f097a6e9acb6281ee34
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-16 19:27:22 -0300

    cria detalhes da apresentação

--- ts/src/app/pagina-principal/main/video/video.component.ts
+++ ts/src/app/pagina-principal/main/video/video.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video.component.css']
 })
 export class VideoComponent implements OnInit {
+  chaves = ['Tipo de Apresentação', 'Eixo temático', 'Palavras-chaves']
+  valores = ['Pôster', 'Alimentação e saúde (AS)', 'Alimentos funcionais, alimentação escolar.']
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
