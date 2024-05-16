de857f7cf2923e6b25f0398d1a61262bdbb212b0
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-16 18:01:19 -0300

    cria section pra o video

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 import {NgOptimizedImage} from "@angular/common";
 import { HeaderComponent } from './pagina-principal/header/header.component';
+import {MainComponent} from "./pagina-principal/main/main.component";
+import {VideoComponent} from "./pagina-principal/main/video/video.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuLateralComponent,
     PaginaPrincipalComponent,
+    MainComponent,
+    VideoComponent,
     HeaderComponent
   ],
     imports: [
