bf29fc90bea4ba12205e62efb13af5b97710a9c6
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-16 15:28:07 -0300

    cria menu lateral

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuLateralComponent,
+    PaginaPrincipalComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
