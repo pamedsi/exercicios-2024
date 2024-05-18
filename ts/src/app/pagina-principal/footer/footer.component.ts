d5299de84f0f2ae752e582d835794b178f8a5dab
Patrick Melo <patrickabimael@yahoo.com.br>
2024-05-18 12:40:38 -0300

    finaliza rodapé

--- ts/src/app/pagina-principal/footer/footer.component.ts
+++ ts/src/app/pagina-principal/footer/footer.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  readonly parteEmNegrito: string;
+  readonly parteSemNegrito: string;
 
-  constructor() { }
+  constructor() {
+    this.parteEmNegrito = this.textoCompleto.split(" gerado")[0]
+    this.parteSemNegrito = this.textoCompleto.replace(this.parteEmNegrito, "")
+  }
 
   ngOnInit(): void {
   }
 
+  readonly textoCompleto = `Preservar a memória do evento e ampliar o acesso ao conhecimento científico gerado em eventos é a razão de ser da plataforma Galoá Proceedings.
+
+Os trabalhos publicados aqui têm maior alcance e ficam disponíveis para toda a comunidade científica, mantendo aceso o debate científico fomentado pelos encontros e aumentando o impacto do evento.`
 }
